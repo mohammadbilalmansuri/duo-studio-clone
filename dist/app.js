33f249b8c2af32ae67deb3c0a8d4969fb6f99830
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2024-12-26 16:01:02 +0530

    Update files

--- dist/app.js
+++ dist/app.js
@@ -1,39 +1,16 @@
+const lenis = new Lenis();
 gsap.registerPlugin(ScrollTrigger, MotionPathPlugin);
+lenis.on("scroll", ScrollTrigger.update);
 
-const locoScroll = new LocomotiveScroll({
-  el: document.querySelector("main"),
-  smooth: true,
-});
-locoScroll.on("scroll", ScrollTrigger.update);
-
-ScrollTrigger.scrollerProxy("main", {
-  scrollTop(value) {
-    return arguments.length
-      ? locoScroll.scrollTo(value, 0, 0)
-      : locoScroll.scroll.instance.scroll.y;
-  },
-
-  getBoundingClientRect() {
-    return {
-      top: 0,
-      left: 0,
-      width: window.innerWidth,
-      height: window.innerHeight,
-    };
-  },
-
-  pinType: document.querySelector("main").style.transform
-    ? "transform"
-    : "fixed",
-});
-
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-ScrollTrigger.refresh();
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
 
 // ------------ Cursor ------------
 
 const cursor = document.querySelector("#cursor");
-
 gsap.set(cursor, { xPercent: -50, yPercent: -50 });
 
 window.addEventListener("mousemove", (e) => {
@@ -46,10 +23,7 @@ window.addEventListener("mousemove", (e) => {
   });
 });
 
-let allMedia = [
-  ...document.querySelectorAll("video"),
-  ...document.querySelectorAll("img"),
-];
+const allMedia = document.querySelectorAll("video, img");
 
 allMedia.forEach((media) => {
   media.addEventListener("mouseenter", () => {
@@ -65,105 +39,99 @@ allMedia.forEach((media) => {
 
 // ------------- Background Color Change ---------------
 
-ScrollTrigger.create({
-  trigger: "#section2",
-  start: "top 65%",
-  end: "top 65%",
-  scroller: "main",
-  scrub: true,
-  toggleActions: "play none none reverse", // toggleActions: "action1 action2 action3 action4"
-  // action1: Animation starts (enters viewport)
-  // action2: Animation ends (leaves viewport)
-  // action3: Animation re-enters (enters from bottom)
-  // action4: Animation reverses (scrolls back past start)
+function setupScrollTrigger(trigger, start, end, onEnter, onLeaveBack) {
+  ScrollTrigger.create({
+    trigger,
+    start,
+    end,
+    scroller: "body",
+    scrub: true,
+    toggleActions: "play none none reverse", // toggleActions: "action1 action2 action3 action4"
+    // action1: Animation starts (enters viewport)
+    // action2: Animation ends (leaves viewport)
+    // action3: Animation re-enters (enters from bottom)
+    // action4: Animation reverses (scrolls back past start)
+    onEnter,
+    onLeaveBack,
+  });
+}
 
-  onEnter: () => {
-    document.body.classList.replace("bg-gray-950", "bg-gray-200");
-    document.body.classList.replace("text-gray-200", "text-gray-950");
+setupScrollTrigger(
+  "#section2",
+  "top 75%",
+  "top 75%",
+  () => {
+    document.body.classList.replace("bg-black", "bg-white");
+    document.body.classList.replace("text-white", "text-black");
   },
-  onLeaveBack: () => {
-    document.body.classList.replace("bg-gray-200", "bg-gray-950");
-    document.body.classList.replace("text-gray-950", "text-gray-200");
+  () => {
+    document.body.classList.replace("bg-white", "bg-black");
+    document.body.classList.replace("text-black", "text-white");
+  }
+);
+
+setupScrollTrigger(
+  "#section4",
+  "top bottom",
+  "top bottom",
+  () => {
+    document.body.classList.replace("bg-white", "bg-black");
+    document.body.classList.replace("text-black", "text-white");
+    const button = document.querySelector("#section3 button");
+    button.classList.remove(
+      "bg-transparent",
+      "border-black",
+      "hover:bg-pink",
+      "hover:border-pink"
+    );
+    button.classList.add(
+      "bg-pink",
+      "text-black",
+      "border-pink",
+      "hover:bg-white",
+      "hover:border-white"
+    );
   },
-});
+  () => {
+    document.body.classList.replace("bg-black", "bg-white");
+    document.body.classList.replace("text-white", "text-black");
+    const button = document.querySelector("#section3 button");
+    button.classList.remove(
+      "bg-pink",
+      "text-black",
+      "border-pink",
+      "hover:bg-white",
+      "hover:border-white"
+    );
+    button.classList.add(
+      "bg-transparent",
+      "border-black",
+      "hover:bg-pink",
+      "hover:border-pink"
+    );
+  }
+);
 
-ScrollTrigger.create({
-  trigger: "#section4",
-  start: "top bottom",
-  end: "top bottom",
-  scroller: "main",
-  scrub: true,
-  toggle0Actions: "play none none reverse",
-  onEnter: () => {
-    document.body.classList.replace("bg-gray-200", "bg-gray-950");
-    document.body.classList.replace("text-gray-950", "text-gray-200");
-    document
-      .querySelector("#section3 button")
-      .classList.remove(
-        "bg-transparent",
-        "border-gray-950",
-        "hover:bg-pink-300",
-        "hover:border-pink-300"
-      );
-    document
-      .querySelector("#section3 button")
-      .classList.add(
-        "bg-pink-300",
-        "text-gray-950",
-        "border-pink-300",
-        "hover:bg-gray-200",
-        "hover:border-gray-200"
-      );
-  },
-  onLeaveBack: () => {
-    document.body.classList.replace("bg-gray-950", "bg-gray-200");
-    document.body.classList.replace("text-gray-200", "text-gray-950");
-    document
-      .querySelector("#section3 button")
-      .classList.remove(
-        "bg-pink-300",
-        "text-gray-950",
-        "border-pink-300",
-        "hover:bg-gray-200",
-        "hover:border-gray-200"
-      );
-    document
-      .querySelector("#section3 button")
-      .classList.add(
-        "bg-transparent",
-        "border-gray-950",
-        "hover:bg-pink-300",
-        "hover:border-pink-300"
-      );
-  },
-});
+// -------------------- Section 1 ------------------------
 
-// -------------------- Section1 animations ------------------------
-
-gsap.from("#section1 h1 span:nth-child(1)", {
-  rotate: -7,
-  opacity: 0,
-  duration: 1,
-});
-
-gsap.from("#section1 h1 span:nth-child(2)", {
+gsap.from("#section1 h1 span:nth-child(1), #section1 h1 span:nth-child(2)", {
   rotate: -7,
   opacity: 0,
   duration: 1,
 });
 
 gsap.from("#section1 video", {
-  opacity: 0,
+  y: 100,
   duration: 1,
 });
 
 gsap.to("#section1 h1", {
   width: "100%",
   scrollTrigger: {
-    trigger: "#section1 h1",
-    scroller: "main",
-    start: "top 25%",
-    end: "top 10%",
+    trigger: "#section1",
+    scroller: "body",
+    start: "top top",
+    end: "top -25%",
     scrub: 1,
   },
 });
@@ -171,15 +139,15 @@ gsap.to("#section1 h1", {
 gsap.to("#section1 video", {
   scale: 1,
   scrollTrigger: {
-    trigger: "#section1 video",
-    scroller: "main",
-    start: "top 75%",
-    end: "top 15%",
+    trigger: "#section1",
+    scroller: "body",
+    start: "top -20%",
+    end: "top -60%",
     scrub: 1,
   },
 });
 
-// -------------------- Section 2 & 3 animations ---------------------
+// -------------------- Section 2 & 3 ---------------------
 
 gsap.from("#section2 h3, #section2 h2, #section2 p, #section2 button", {
   y: 100,
@@ -188,7 +156,7 @@ gsap.from("#section2 h3, #section2 h2, #section2 p, #section2 button", {
   stagger: 0.2,
   scrollTrigger: {
     trigger: "#section2",
-    scroller: "main",
+    scroller: "body",
     start: "top 60%",
     end: "top 60%",
   },
@@ -201,7 +169,7 @@ gsap.from("#section3 h2, #section3 .work, #section3 .cta", {
   stagger: 0.5,
   scrollTrigger: {
     trigger: "#section3",
-    scroller: "main",
+    scroller: "body",
     start: "top 60%",
     end: "top 60%",
   },
@@ -216,7 +184,7 @@ gsap.from("#section4 h4, #section4 h2", {
   stagger: 0.3,
   scrollTrigger: {
     trigger: "#section4",
-    scroller: "main",
+    scroller: "body",
     start: "top center",
     end: "top center",
   },
@@ -257,7 +225,6 @@ titles.forEach((title) => {
 });
 
 // --------------- section 5 ---------------
-
 gsap.from("#section5 h2, #section5 button, .clients", {
   y: 100,
   opacity: 0,
@@ -265,34 +232,29 @@ gsap.from("#section5 h2, #section5 button, .clients", {
   stagger: 0.3,
   scrollTrigger: {
     trigger: "#section5",
-    scroller: "main",
+    scroller: "body",
     start: "top center",
     end: "top center",
   },
 });
 
-const clients = document.querySelectorAll(".clients div");
+const clients = document.querySelectorAll(".client");
 
 clients.forEach((client) => {
-  client.addEventListener("mouseenter", (e) => {
+  client.addEventListener("mouseenter", () => {
     cursor.classList.remove("size-4", "rounded-full", "mix-blend-difference");
-    cursor.classList.add("w-96", "h-[16rem]", "object-cover", "object-center");
+    cursor.classList.add("w-96", "h-64", "object-cover", "object-center");
     cursor.style.backgroundImage = `url(${client.getAttribute("data-image")})`;
   });
 
-  client.addEventListener("mouseleave", (e) => {
+  client.addEventListener("mouseleave", () => {
     cursor.style.backgroundImage = "none";
-    cursor.classList.remove(
-      "w-96",
-      "h-[16rem]",
-      "object-cover",
-      "object-center"
-    );
+    cursor.classList.remove("w-96", "h-64", "object-cover", "object-center");
     cursor.classList.add("size-4", "rounded-full", "mix-blend-difference");
   });
 });
 
-// ----------- Magnetic effect in buttons ------------
+// ----------- Magnetic effect ------------
 
 const magnetize = (
   element,
@@ -336,28 +298,10 @@ const magnetize = (
   });
 };
 
-document.querySelectorAll(".magnetic").forEach((button) => {
-  magnetize(button);
-});
-
-// --------- Footer Parallex Effect ---------
-
-const footer = document.querySelector("footer");
-footer.style.position = "absolute";
-footer.style.left = 0;
-footer.style.bottom = `-${footer.offsetHeight}px`;
-
-gsap.to(footer, {
-  bottom: 0,
-  ease: "power4.out",
-  scrollTrigger: {
-    trigger: "#section5",
-    scroller: "main",
-    start: "bottom bottom",
-    end: `bottom ${window.innerHeight - footer.offsetHeight}`,
-    scrub: 2,
-    pin: true,
-  },
-});
+if (window.innerWidth > 1080) {
+  document.querySelectorAll(".magnetic").forEach((button) => {
+    magnetize(button);
+  });
+}
 
 ScrollTrigger.refresh();
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ["./dist/**/*.{html,js}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#e5e7eb",
+      black: "#030712",
+      gray: "#6b7280",
+      pink: "#f9a8d4",
+    },
+    screens: {
+      xs: "480px",
+      sm: "680px",
+      md: "880px",
+      lg: "1080px",
+      xl: "1280px",
+      "2xl": "1480px",
+      "3xl": "1680px",
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
